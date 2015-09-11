819503cb84e969270602bd2c5dc5233695ca466c
Si <siwang22@gmail.com>
2015-09-11 00:04:12 -0700

    getElementsByClassName works for childNodes of <body>

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,21 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function (className) {
+  var result = [];
+  var i;
+  var elements = document.body.childNodes;
+
+  for (i = 0; i < elements.length; i++) {
+    if (elements[i].classList !== undefined && elements[i].classList.contains(className)) {
+      result.push(document.body.childNodes[i]);
+    }
+  }
+  //parse through every element in document.body.childNodes
+  //parse through every childNode in the children of <body>
+  //return an array of elements that match className
+  return result;
 };
+
+
+

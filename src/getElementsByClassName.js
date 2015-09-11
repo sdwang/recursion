5ae5ee3406aaf9d4bbf489e0d336616670b8f738
Si <siwang22@gmail.com>
2015-09-11 01:27:11 -0700

    Finished getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,43 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName2 = function (className) {
-  debugger
-  var result = [];
-  var i;
-  var elements = document.body.childNodes;
-
-
-  for (i = 0; i < elements.length; i++) {
-    if (elements[i].classList !== undefined && elements[i].classList.contains(className)) {
-      result.push(elements[i]);
-    }
-  }
-  //parse through every element in document.body.childNodes
-  //parse through every childNode in the children of <body>
-  //return an array of elements that match className
-  return result;
+var getElementsByClassName = function (className) {
+  return getElementsHelper(className, document);
 };
 
 
 var getElementsHelper = function (className, element) {
-  debugger
   var matches = [];
+  //Checks to see if the current element matches the className
   if (element.classList !== undefined && element.classList.contains(className)) {
     matches = matches.concat(element);
   }
+  //Checks to see if current element has children
   if (element.childNodes.length !== 0) {
     var i;
+    //Runs getElementsHelper on every child node
     for (i = 0; i < element.childNodes.length; i++) {
       matches = matches.concat(getElementsHelper(className, element.childNodes[i]));
     }
   }
   return matches;
 };
-
-var getElementsByClassName = function (className) {
-
-
-
-
-}
